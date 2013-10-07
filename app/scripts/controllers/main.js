8ea519bf4975ea2bc77afa5f3fb18ff103ae68f2
Zuyang Kou <leafduo@gmail.com>
2013-10-08 00:45:52 +0800

    Revert to 2 spaces indentation and pass jshint

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,32 +1,34 @@
 'use strict';
+/* global marked:false' */
+/* global hljs:false' */
 
 angular.module('sitdownApp')
   .controller('MainCtrl', function ($scope, storage) {
-      marked.setOptions({
-          gfm: true,
-          highlight: function (code, lang) {
-              if (lang) {
-                  if (lang === 'objc') {
-                      lang = 'objectivec'; //TODO: a mapping here
-                  }
-                  return hljs.highlight(lang, code).value;
-              } else {
-                  hljs.highlightAuto(code).value;
-              }
-          },
-          tables: true,
-          breaks: false,
-          pedantic: false,
-          sanitize: true,
-          smartLists: true,
-          smartypants: false,
-          langPrefix: 'lang-'
-      });
+    marked.setOptions({
+      gfm: true,
+      highlight: function (code, lang) {
+        if (lang) {
+          if (lang === 'objc') {
+            lang = 'objectivec'; //TODO: a mapping here
+          }
+          return hljs.highlight(lang, code).value;
+        } else {
+          return hljs.highlightAuto(code).value;
+        }
+      },
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: true,
+      smartLists: true,
+      smartypants: false,
+      langPrefix: 'lang-'
+    });
 
-      $scope.parseMarkdown = function() {
-          $scope.markdownHTML = marked($scope.markdown);
-      }
+    $scope.parseMarkdown = function() {
+      $scope.markdownHTML = marked($scope.markdown);
+    };
 
-      storage.bind($scope, 'markdown');
-      $scope.parseMarkdown();
+    storage.bind($scope, 'markdown');
+    $scope.parseMarkdown();
   });
